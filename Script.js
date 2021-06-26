0d4b9e1a159cc6288201f0a29591051208c1b72e
Zijian Wang <zw@zijiandembp.myfiosgateway.com>
2021-06-26 14:35:04 -0400

    Added another practice

--- Script.js
+++ Script.js
@@ -1,9 +1,42 @@
-let item1 = document.getElementById("item1");
-// item1.textContent = "Hello?";
-let em1 = document.getElementsByTagName("em");
+let firstCard = 0;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
+let sumEl = document.querySelector("#sum-el");
+console.log(sumEl);
 
-let i = 0;
-while (i<em1.length){
-    em1[i].textContent = "yesyesyes";
-    i++;
+let startGame = () => {    
+    if (sum <=20){
+        message = "Do you want to draw a card?💁🏻"
+    }else if (sum === 21){
+        message = "Wohoo! You've got Blackjack!🥳"    
+    }else{
+        message = "You're out of the game!😇"
+        isAlive =false;    
+    }
+
+    sumEl.textContent = "Sum: " + sum;
+    document.getElementById("message-el").textContent = message;
+}
+let listItem2 = document.getElementById("list2");
+let list = document.getElementsByTagName("ul")[0];
+let newItemLast = document.createElement("li");
+let newTextLast = document.createTextNode("Hahaha, I am new!🤓");
+newItemLast.appendChild(newTextLast);
+console.log(newItemLast);
+list.insertBefore(newItemLast,listItem2);
+
+let listItems = document.querySelectorAll("li");
+
+
+let clickIt = () => {
+for (let i = 0;i<listItems.length;i++) {
+    if (listItems[i].hasAttribute("class")){
+    listItems[i].className = "gla";
+    }else {
+        listItems[i].setAttribute("class","gla");
+    }  
+}
 }
